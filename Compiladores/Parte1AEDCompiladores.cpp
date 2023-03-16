dd2d032c8cd5dc75a4ba6f2f5673bf6ba568faa4
Quezad4 <105978156+Quezad4@users.noreply.github.com>
2023-03-16 16:17:29 -0300

    Create Parte1AEDCompiladores.cpp

---
+++ Compiladores/Parte1AEDCompiladores.cpp
@@ -0,0 +1,97 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+
+class analisadorLexico{
+
+    public:
+    //----------------------------------- Atributos ----------------------------------------------//
+    string texto;
+    vector<string> textoPorLinha;
+
+    // ----------------------------------- Métodos -----------------------------------------------//
+    void leArquivo(){
+        ifstream arquivo;
+        string nomeDoArquivo = "/home/puc/teste.txt";
+        arquivo.open(nomeDoArquivo.c_str(),std::ios::out|std::ios::app);
+        string linha, texto = "";
+        getline(arquivo,linha);
+        while(!arquivo.eof()){
+            texto += linha;
+            texto += '\n';
+            getline(arquivo,linha);
+        }
+        getline(arquivo,linha);
+        texto += linha;
+        texto += '\n';
+        arquivo.close();
+        this->texto = texto;
+        string aux = ""; 
+        for(int i = 0; i<texto.size(); i++){
+            aux+=texto[i];
+            if(texto[i] == '\n'){
+                textoPorLinha.push_back(aux);
+                aux = "";
+            }
+        }
+        
+    };
+     void limpaArquivo(){
+        string result = "";
+        bool comentario = false;
+        bool comentario2 = false;
+        int espaco = 0;
+
+        for(int i = 0; i<textoPorLinha.size(); i++){
+            for(int j = 0; j<textoPorLinha[i].size(); j++){
+                if(textoPorLinha[i][j] == '\n' and comentario){
+                    comentario = false;
+                    continue;
+                }
+                if(textoPorLinha[i][j] == '*' and textoPorLinha[i][j+1] == '/'){
+                    comentario2 = false;
+                    j+=2;
+                    continue;
+                } 
+                
+                if(textoPorLinha[i][j] == ' ') espaco++;
+                if(textoPorLinha[i][j] != ' ') espaco = 0;
+
+                if(textoPorLinha[i][j] == '/' and textoPorLinha[i][j+1] == '/') comentario = true;
+                if(textoPorLinha[i][j] == '/' and textoPorLinha[i][j+1] == '*') comentario2 = true;
+
+                
+                if((textoPorLinha[i][j] != '\n' or textoPorLinha[i].size() != 1) and (comentario != true) and (comentario2 != true) and espaco < 2){
+                    result+= textoPorLinha[i][j];
+                }
+                
+            }
+            
+        }
+        this->texto = result;
+     };
+     void imprimeTextoArquivo(){
+        ofstream arquivo2;
+            string nomeDoArquivo2 = "/home/puc/teste1.txt";
+            arquivo2.open(nomeDoArquivo2.c_str(),std::ios::out|std::ios::app);
+            arquivo2 << texto << endl;
+            arquivo2.close();
+     };
+
+};
+
+
+
+int main(){
+    analisadorLexico obj;
+    obj.leArquivo();
+    //cout << obj.texto << endl;
+    obj.limpaArquivo();
+    //cout << obj.texto << endl;
+    obj.imprimeTextoArquivo();
+   
+   
+    
+}
