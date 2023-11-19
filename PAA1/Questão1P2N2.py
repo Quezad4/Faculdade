5200709ca1f22278d95fa75ee537caae0f961b7b
Mateus Quezada <105978156+Quezad4@users.noreply.github.com>
2023-11-19 13:04:42 -0300

    Update Questão1P2N2.py

--- PAA1/Questão1P2N2.py
+++ PAA1/Questão1P2N2.py
@@ -1,1 +1,28 @@
+def quicksort(arr, low, high):
+    if low < high:  
+        pi = partition(arr, low, high)  
+        quicksort(arr, low, pi - 1)  
+        quicksort(arr, pi + 1, high)  
 
+def partition(arr, low, high):
+    pivot = arr[high]
+    i = low - 1  
+
+    for j in range(low, high):  
+        if arr[j] <= pivot:  
+            i += 1  
+            arr[i], arr[j] = arr[j], arr[i]  
+
+    arr[i + 1], arr[high] = arr[high], arr[i + 1]  
+    return i + 1  
+
+
+def main():
+    vetorTest = [10,2,100,200,111,4,5,6,9,12,66,31,78,65,38,91]
+    quicksort(vetorTest, 0, len(vetorTest)-1)
+    print(vetorTest)
+
+
+
+if __name__ == "__main__":
+    main()
